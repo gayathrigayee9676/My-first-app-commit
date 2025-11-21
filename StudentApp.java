b15701702cb3c5f13ed9c844a9fd8569a6feeef0
Gayathri <gayathrigayee9676@gmail.com>
2025-11-21 12:37:01 -0500

    HIS-300:Related changes

--- StudentApp.java
+++ StudentApp.java
@@ -1,5 +1,6 @@
 public class StudentApp{
 public static void main (String[]args){
+private static final String RESt _URL="https://www.equifax.com/getScores
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter student name:");
 String name=sc.nextLine();
